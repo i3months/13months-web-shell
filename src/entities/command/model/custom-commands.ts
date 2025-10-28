4067117e72e8c51694fd3c69811cb509041c4f3f
jmj <waysoleward01@naver.com>
2025-10-28 11:17:59 +0900

    feat: add some custom command

--- src/entities/command/model/custom-commands.ts
+++ src/entities/command/model/custom-commands.ts
@@ -2,9 +2,30 @@ import { CustomCommand } from "./types";
 
 export const customCommands: CustomCommand[] = [
   {
+    name: "github",
+    type: "link",
+    value: "https://github.com/i3months",
+    description: "Open GitHub profile",
+  },
+  {
+    name: "resume",
+    type: "link",
+    value: "https://13months.netlify.app/en",
+    description: "Open resume website",
+  },
+  {
+    name: "contact",
+    type: "text",
+    value: `- Linkedin : https://www.linkedin.com/in/joonmo-jeong/
+- Github   : https://github.com/i3months
+- Blog     : https://13months.tistory.com
+- Email    : jeongjoonmo.dev@gmail.com`,
+    description: "Display contact information",
+  },
+  {
     name: "linkedin",
     type: "link",
-    value: "https://linkedin.com/in/13months",
+    value: "https://www.linkedin.com/in/joonmo-jeong/",
     description: "Open LinkedIn profile",
   },
   {
